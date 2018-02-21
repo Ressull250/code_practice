5159a62ffeae26588aa9f614001a38187c082665
zyb <zyb250250@qq.com>
2018-02-21 11:18:09 +0800

    update question33

---
+++ part2/33.py
@@ -0,0 +1,24 @@
+import sys
+class Solution:
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        l,r = 0,len(nums)-1
+        while l<=r:
+            mid = l+(l-r)//2
+            num = nums[mid] if (target<nums[0]) == (nums[mid]<nums[0]) \
+                    else (sys.maxsize if target>nums[0] else -sys.maxsize)
+
+            if num < target:
+                l = mid+1
+            elif num > target:
+                r = mid+1
+            else:
+                return mid
+
+        return -1
+
+print(Solution().search([3,1],2))

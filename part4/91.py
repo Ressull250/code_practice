18499475b2d9b5925cf39aab5f982ce849b8bbf9
zyb <zyb250250@qq.com>
2018-07-23 20:00:11 +0800

    add P1484 没有仔细看题，10用X代替（

--- part4/91.py
+++ part4/91.py
@@ -1,20 +1,29 @@
+# -*- coding:utf-8 -*-
 class Solution(object):
     def numDecodings(self, s):
         """
         :type s: str
         :rtype: int
         """
-        res = [0]
         dp = [0 for _ in range(len(s)+1)]
-        for i in range(len(s)):
-            if s[i] != '0':
-                dp[i+1] = dp[i]+1
-            if i-1>=0 and 10 <= int(s[i-1:i+1]) <= 26:
-                print(int(s[i-1:i+1]))
-                dp[i+1] = dp[i-1]+1
+        if s[0] == '0': return 0
+        dp[0] = dp[1] = 1
+        for i in range(1,len(s)):
+            if s[i] == '0':
+                if s[i-1] == '1' or s[i-1] == '2':
+                    dp[i+1] = dp[i-1]
+                else:
+                    return 0
+            else:
+                if 0<int(s[i-1:i+1])<=9:
+                    dp[i+1] = dp[i-1]
+                elif 10<=int(s[i-1:i+1])<=26:
+                    dp[i+1] = dp[i] + dp[i-1]
+                else:
+                    dp[i+1] = dp[i]
 
-        print(dp[len(s)])
-        return dp
+        return dp[len(s)]
 
 
-print(Solution().numDecodings('2611'))
+
+print(Solution().numDecodings('101'))

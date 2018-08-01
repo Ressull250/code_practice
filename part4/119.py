ac605ed2f1548180d25f096d01a78d00aadd91d4
zyb <zyb250250@qq.com>
2018-08-01 19:35:30 +0800

    add 118 水,现在窗外正在下暴雨

---
+++ part4/119.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def getRow(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        res = []
+        for i in range(1,numRows+2):
+            ans = [1 for _ in range(i)]
+            for j in range(1,i-1):
+                ans[j] = res[i-2][j-1] + res[i-2][j]
+            res.append(ans)
+        return res[-1]
+
+

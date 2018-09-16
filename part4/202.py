872cdb9a5f84385e60049b03c99313b032192b1a
zyb <zyb250250@qq.com>
2018-09-16 21:34:09 +0800

    add 202 循环的事都可以双指针tql

--- part4/202.py
+++ part4/202.py
@@ -1,4 +1,4 @@
-class Solution(object):
+class Solution1(object):
     def isHappy(self, n):
         """
         :type n: int
@@ -22,5 +22,32 @@ class Solution(object):
 
         table = []
         return next(n)
+
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+
+        def next(num):
+            sum = 0
+            while num != 0:
+                val = num % 10
+                sum += val*val
+                num /= 10
+            return sum
+
+        slow = fast = n
+        while True:
+            slow = next(slow)
+            if slow == 1:
+                return True
+            fast = next(fast)
+            fast = next(fast)
+            if slow == fast:
+                break
+
+        return False
 
 print Solution().isHappy(19)
